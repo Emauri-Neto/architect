935ac9fdd723569eabad203eb2bb3aa880074591
Emauri Neto <emaurigomesneto@gmail.com>
2026-06-14 18:15:59 -0300

    [feat] add theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { HashRouter, Routes, Route } from "react-router"
 import HomePage from "./pages/Home";
 import "./App.css";
+import { ThemeProvider } from "./components/theme";
 
 function App() {
   return <HashRouter>
-    <Routes>
-      <Route index path="/" element={<HomePage/>}/>
-    </Routes>
+    <ThemeProvider defaultTheme="dark" storageKey="architect-ui-theme">
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+      </Routes>
+    </ThemeProvider>
   </HashRouter>
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,4 +2,4 @@ import { Button } from "@/components/ui/button";
 
 export default function HomePage(){
     return <Button>asdf</Button>
-}
+}   
