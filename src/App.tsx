4f9b6ad0d65f1728169e068974a3d1a64f5a5913
Emauri Neto <emaurigomesneto@gmail.com>
2026-06-14 17:58:04 -0300

    [feat] add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { HashRouter, Routes, Route } from "react-router"
+import HomePage from "./pages/Home";
 import "./App.css";
 
 function App() {
-  return <>
-  </>
+  return <HashRouter>
+    <Routes>
+      <Route index path="/" element={<HomePage/>}/>
+    </Routes>
+  </HashRouter>
 }
 
 export default App;
